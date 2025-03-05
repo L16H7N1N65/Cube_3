7510ce88ae5bb618262b8f1ee5b53ba5d04bfd1e
Linda Meghouche <l.meghouche@tkminnovation.io>
2025-03-05 16:24:34 +0100

    db setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,10 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 5432,  
   dialect: 'postgres',
-  logging: false, // Désactive les logs Sequelize
+  logging: false,
 });
 
 module.exports = sequelize;
+
