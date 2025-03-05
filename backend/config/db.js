c10b48ecb04ceece135a865cee58b29d6fd893d5
Mekkilangelo <mekki.abdessatar@gmail.com>
2025-03-05 14:40:02 +0100

    complete models and associations

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,21 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const { Sequelize, DataTypes } = require('sequelize');
+require('dotenv').config(); // Charger les variables d'environnement
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_NAME, // Nom de la base de données
+  process.env.DB_USER, // Utilisateur
+  process.env.DB_PASSWORD, // Mot de passe
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
-    dialect: 'postgres',
-    logging: false,
+    port: process.env.DB_PORT,
+    dialect: 'postgres', // Utilisation de PostgreSQL
+    logging: false, // Désactiver les logs SQL dans la console
   }
 );
 
-module.exports = sequelize;
+// Vérifier la connexion à la base de données
+sequelize.authenticate()
+  .then(() => console.log('✅ Connexion réussie à PostgreSQL'))
+  .catch(err => console.error('❌ Erreur de connexion à PostgreSQL:', err));
+
+  module.exports = { sequelize, DataTypes };
