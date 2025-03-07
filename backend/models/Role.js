e090704e860838e23ef082ddd9ab9ec689621348
Mekkilangelo <mekki.abdessatar@gmail.com>
2025-03-07 09:46:16 +0100

    change role.js as user.js

--- backend/models/Role.js
+++ backend/models/Role.js
@@ -1,23 +1,19 @@
-const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/db');
-
-const Role = sequelize.define('Role', {
-  role_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  description: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  }
-}, {
-  tableName: 'roles',
-  timestamps: true
-});
-
-module.exports = Role;
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define('Role', {
+    role_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    }
+  }, {
+    timestamps: true
+  });
+};
